a46fca48666d771d19ea3a40a6ed2e336978ecfb
Tim Aclan <aclantimothy@gmail.com>
2025-08-27 17:32:43 -0500

    Added a bottom tab navigator by adding a (tabs) subdirectory inside the app directory, creating a _layout.tsx inside the (tabs) subdirectory, moving the existing index.tsx and about.tsx inside the (tabs) subdirectory, updating the Root layout file to add a (tabs) route, and adding a Tabs component to define the bottom tab layout inside (tabs)/_layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,7 @@ export default function RootLayout()
 {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="about" options={{ title: 'About' }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
