629fdc088947975b9e232b57d18448d6e5b2adbc
Tim Aclan <aclantimothy@gmail.com>
2025-08-28 16:51:23 -0500

    Displayed the image by installing the expo-image library, importing Image from the expo-image library, and creating a PlaceholderImage variable to use assets/images/background-image.png file as the source prop on the Image component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,15 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import { View, StyleSheet } from 'react-native';
+import { Image } from 'expo-image';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() 
 {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
     </View>
   );
 }
@@ -19,16 +20,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: 
+  imageContainer: 
   {
-    color: '#fff',
+    flex: 1,
   },
-  button: 
+  image: 
   {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
