a7f5797aba87b549b5b505835221c0abab0ec016
Tim Aclan <aclantimothy@gmail.com>
2025-08-28 17:43:02 -0500

    Created buttons using Pressable by creating Button.tsx inside the components directory and importing the component into app/(tabs)/index.tsx as well as adding styles for the <View> that encapsulates the buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { View, StyleSheet } from 'react-native';
 
+import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
@@ -10,6 +11,10 @@ export default function Index()
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -25,5 +30,10 @@ const styles = StyleSheet.create({
   imageContainer: 
   {
     flex: 1,
+  },
+  footerContainer: 
+  {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
