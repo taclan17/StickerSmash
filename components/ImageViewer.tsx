55c2b8c5b1e3cc1f1be968e45670bcfd40edcc08
Tim Aclan <aclantimothy@gmail.com>
2025-08-29 19:24:14 -0500

    Passed imageSource as the value of the source prop on the Image component

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -11,7 +11,7 @@ export default function ImageViewer({ imgSource, selectedImage }: Props)
 {
   const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
   
-  return <Image source={imgSource} style={styles.image} />;
+  return <Image source={imageSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
