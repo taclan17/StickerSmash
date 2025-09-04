1aabafdb4597e24115d1cc516116bacae93d1dc0
Tim Aclan <aclantimothy@gmail.com>
2025-09-04 18:04:28 -0500

    Created a doubleTap object to scale the initial value and use Gesture.Tap() to animate the transition while scaling the sticker image

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -11,6 +11,19 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props)
 {
   const scaleImage = useSharedValue(imageSize);
 
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2)
+      {
+        scaleImage.value = scaleImage.value * 2;
+      }
+      else
+      {
+        scaleImage.value = Math.round(scaleImage.value / 2);
+      }
+    });
+
   return (
     <View style={{ top: -350 }}>
       <Animated.Image 
