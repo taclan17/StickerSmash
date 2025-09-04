dd3258dcded29038a5284752ba5c135d14755269
Tim Aclan <aclantimothy@gmail.com>
2025-09-03 21:10:44 -0500

    Created an isModalVisible state variable with the useState hook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,6 +14,7 @@ export default function Index()
 {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
   const pickImageAsync = async () =>
   {
