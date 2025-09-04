68917206903e234d61bb020851b26abd6c89108f
Tim Aclan <aclantimothy@gmail.com>
2025-09-04 17:14:08 -0500

    Rendered <GestureHandlerRootView> from react-native-gesture-handler at the top of Index component and replaced the root level <View> component in the app/(tabs)/index.tsx with <GestureHandlerRootView>

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,6 +10,8 @@ import EmojiPicker from '@/components/EmojiPicker';
 import EmojiList from '@/components/EmojiList';
 import EmojiSticker from '@/components/EmojiSticker';
 
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() 
@@ -59,7 +61,7 @@ export default function Index()
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
         {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
@@ -81,7 +83,7 @@ export default function Index()
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
