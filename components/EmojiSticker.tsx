35a55c92ae72206da83fd4712cf81e154d6ef114
Tim Aclan <aclantimothy@gmail.com>
2025-09-04 19:17:37 -0500

    Updated the <EmojiSticker> component so that the <GestureDetector> component becomes the top-level component

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -52,14 +52,16 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props)
   });
 
   return (
-    <Animated.View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image 
-          source={stickerSource} 
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]} 
-        />
-      </GestureDetector>
-    </Animated.View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={{ top: -350 }}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image 
+            source={stickerSource} 
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]} 
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 }
