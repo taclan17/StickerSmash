ae7c6e1a9ebbfe55ffc9d87e28496de1e410d18f
Tim Aclan <aclantimothy@gmail.com>
2025-09-11 17:32:58 -0500

    Grouped the StatusBar and existing Stack components with React's Fragment component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,11 @@ import { StatusBar } from 'expo-status-bar';
 export default function RootLayout() 
 {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
